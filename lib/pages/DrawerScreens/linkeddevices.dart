f8aaa01ea05cdd6c2ff8344f8cfc0ebb02f3f7ef
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-18 10:10:14 +0530

    linked devices page

--- lib/pages/DrawerScreens/linkeddevices.dart
+++ lib/pages/DrawerScreens/linkeddevices.dart
@@ -23,11 +23,94 @@ class _LinkedDevicesPageState extends State<LinkedDevicesPage> {
         title: Text('Linked devices',style: TextStyle(color: Colors.white),),
       ),
       body:Container(
-        height: srcheight*0.4 ,
+        color: Color.fromRGBO(18,24,29,1.0),
+        height: double.infinity,
         width: double.infinity,
-        color: Color.fromARGB(255,11,17,21),
+        child: Column(
+          children: [
+            Container(
+              height: srcheight*0.4 ,
+              width: double.infinity,
+              color: Color.fromARGB(255,11,17,21),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  // Image.asset('assets/linkeddevices.png',color: Colors.red,),
+                  Text('You can link other devices to this account.',style: TextStyle(color: Colors.grey),),
+                  Text('Learn More',style: TextStyle(color: Colors.blue),),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(80,255,50,1.0)
+                    ),
+                      onPressed: (){},
+                      child: Text('Link a device',style: TextStyle(color: Colors.black),)),
+
+                ],
+              ),
+
+
+
+
+            ),
+            Divider(
+              // height: srcwidth*0.1,
+              thickness: srcwidth*0.05,
+
+              color: Color.fromRGBO(18,24,29,1.0),),
+            Container(
+              height: srcheight*0.17 ,
+              width: double.infinity,
+              color: Color.fromARGB(255,11,17,21),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Device Status.',style: TextStyle(color: Colors.grey),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Text('Tap a device to log out',style: TextStyle(color: Colors.grey),),
+                  ),
+                  //todo streambuilder that fetches from user on what devices is this whatsapp logged in
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Icon(Icons.window_sharp),
+                    ),
+                    title: Text('Windows',style: TextStyle(color: Colors.white),),
+                    subtitle: Text('Last active today at 9:27 PM',style: TextStyle(color: Colors.grey),),
+                  ),
+
+
+
+                ],
+              ),
+            ),
+            Icon(Icons.lock, color: Colors.grey),
+            RichText(text: TextSpan(
+              style:TextStyle(fontSize: srcwidth*0.1)
+                  ,children: [
+              TextSpan(
+              text: 'Your personal',
+              style: TextStyle(color: Colors.grey),
+            ),
+              TextSpan(
+                text: 'Green Text ',
+                style: TextStyle(color: Colors.green),
+              ),
+              TextSpan(
+                text: 'More Grey Text',
+                style: TextStyle(color: Colors.grey),
+              ),
+
+            ))
+          ],
+        ),
 
       ),
+
 
 
 
