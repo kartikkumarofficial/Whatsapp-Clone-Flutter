4cfee0cb08807c991b0a2ff83c6c76a41b27df49
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-25 17:53:15 +0530

    settings

---
+++ lib/pages/DrawerScreens/settings/Help/Help.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class HelpSettingsPage extends StatefulWidget {
+  const HelpSettingsPage({super.key});
+
+  @override
+  State<HelpSettingsPage> createState() => _HelpSettingsPageState();
+}
+
+class _HelpSettingsPageState extends State<HelpSettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    var srcheight = MediaQuery.of(context).size.height;
+    var srcwidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255,11,17,21),
+        title: Padding(
+          padding: const EdgeInsets.only(top: 3),
+          child: const Text(
+            'Profile',
+            style: TextStyle(
+              fontSize:23,
+              color: Colors.white,
+            ),
+          ),
+        ),
+
+      ),
+      body: Container(
+        color:Color.fromARGB(255,11 ,17,21),
+        height: double.infinity,
+        width: double.infinity,
+        child: ListView(
+          // physics: const BouncingScrollPhysics(),
+          children: [
+            Divider(
+              thickness: 0.5,
+              color: Color.fromARGB(255, 31, 43, 50),
+            ),
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
